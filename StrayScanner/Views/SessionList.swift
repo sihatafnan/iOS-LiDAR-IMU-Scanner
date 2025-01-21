28d497555fc7f3da35f39c21192919c56370d947
Sihat Afnan <sihatafnan@Sihats-MacBook-Pro.local>
2025-01-21 00:31:47 -0800

    automated recording

--- StrayScanner/Views/SessionList.swift
+++ StrayScanner/Views/SessionList.swift
@@ -12,11 +12,13 @@ import CoreData
 class SessionListViewModel: ObservableObject {
     private var dataContext: NSManagedObjectContext?
     @Published var sessions: [Recording] = []
+    @Published var sessionCount: Int = 0 // New property for in-memory count
 
     init() {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
         dataContext = appDelegate.persistentContainer.viewContext
         self.sessions = []
+        sessionCount = 0 // Reset count on initialization
         NotificationCenter.default.addObserver(self, selector: #selector(sessionsChanged), name: NSNotification.Name("sessionsChanged"), object: nil)
     }
 
@@ -31,7 +33,7 @@ class SessionListViewModel: ObservableObject {
             sessions = fetched.map { session in
                 return session as! Recording
             }
-
+//            sessionCount = 0 // Reset the count when fetching sessions
         } catch let error as NSError {
             print("Something went wrong. Error: \(error), \(error.userInfo)")
         }
@@ -39,6 +41,7 @@ class SessionListViewModel: ObservableObject {
 
     @objc func sessionsChanged() {
         fetchSessions()
+        sessionCount += 1
     }
 
 }
@@ -46,6 +49,8 @@ class SessionListViewModel: ObservableObject {
 struct SessionList: View {
     @ObservedObject var viewModel = SessionListViewModel()
     @State private var showingInfo = false
+    @State private var autoNavigate = false // Add a flag for automatic navigation
+    
 
     init() {
         UITableView.appearance().backgroundColor = UIColor(named: "BackgroundColor")
@@ -53,92 +58,89 @@ struct SessionList: View {
 
     var body: some View {
         ZStack {
-        Color.black
-        .ignoresSafeArea()
-        NavigationView {
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("Recordings")
-                        .foregroundColor(Color("TextColor"))
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                        .padding([.top, .leading], 15.0)
-                    Spacer()
-                    Button(action: {
-                        showingInfo.toggle()
-                    }, label: {
-                        Image(systemName: "info.circle")
-                            .resizable()
-                            .frame(width: 25, height: 25, alignment: .center)
-                            .padding(.top, 17)
-                            .padding(.trailing, 20)
+            Color.black
+                .ignoresSafeArea()
+            NavigationView {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Recordings")
                             .foregroundColor(Color("TextColor"))
-                    }).sheet(isPresented: $showingInfo) {
-                        InformationView()
-                    }
-                }
-                
-                // Add a Text view for the total recording count
-                if !viewModel.sessions.isEmpty {
-                    Text("Total Recordings: \(viewModel.sessions.count)")
-                        .foregroundColor(Color("TextColor"))
-                        .font(.body)
-                        .fontWeight(.medium)
-                        .padding([.leading, .top], 15.0)
-                }
-
-                if !viewModel.sessions.isEmpty {
-                    List {
-                        ForEach(Array(viewModel.sessions.enumerated()), id: \.element) { i, recording in
-                            NavigationLink(destination: SessionDetailView(recording: recording)) {
-                                SessionRow(session: recording)
-                            }
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                            .padding([.top, .leading], 15.0)
+                        Spacer()
+                        Button(action: {
+                            showingInfo.toggle()
+                        }, label: {
+                            Image(systemName: "info.circle")
+                                .resizable()
+                                .frame(width: 25, height: 25, alignment: .center)
+                                .padding(.top, 17)
+                                .padding(.trailing, 20)
+                                .foregroundColor(Color("TextColor"))
+                        }).sheet(isPresented: $showingInfo) {
+                            InformationView()
                         }
                     }
-                    Spacer()
-                } else {
-                    Spacer()
-                    Text("No recorded sessions. Record one, and it will appear here.")
-                        .font(.body)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 50.0)
+
+                    if !viewModel.sessions.isEmpty {
+                        Text("Total Recordings in Current Session: \(viewModel.sessionCount)")
+                            .foregroundColor(Color("TextColor"))
+                            .font(.body)
+                            .fontWeight(.medium)
+                            .padding([.leading, .top], 15.0)
+                    }
+
+                    if !viewModel.sessions.isEmpty {
+                        List {
+                            ForEach(Array(viewModel.sessions.enumerated()), id: \.element) { i, recording in
+                                NavigationLink(destination: SessionDetailView(recording: recording)) {
+                                    SessionRow(session: recording)
+                                }
+                            }
+                        }
+                        Spacer()
+                    } else {
+                        Spacer()
+                        Text("No recorded sessions. Record one, and it will appear here.")
+                            .font(.body)
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 50.0)
+                    }
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: NewSessionView(), isActive: $autoNavigate) { // Trigger navigation programmatically
+                            Text("Record new session")
+                                .font(.title3)
+                                .padding(20)
+                                .background(Color("TextColor"))
+                                .foregroundColor(Color("LightColor"))
+                                .cornerRadius(35)
+                                .padding(20)
+                        }
+                        Spacer()
+                    }
+                    if (viewModel.sessions.isEmpty) {
+                        Spacer()
+                    }
                 }
-                HStack {
-                    Spacer()
-                    NavigationLink(destination: NewSessionView(), label: {
-                        Text("Record new session")
-                            .font(.title3)
-                            .padding(20)
-                            .background(Color("TextColor"))
-                            .foregroundColor(Color("LightColor"))
-                            .cornerRadius(35)
-                            .padding(20)
-                    })
-                    Spacer()
-                }
-                if (viewModel.sessions.isEmpty) {
-                    Spacer()
+                .navigationBarHidden(true)
+                .background(Color("BackgroundColor").ignoresSafeArea())
+                .onAppear {
+                    DispatchQueue.main.async {
+                        viewModel.fetchSessions()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            autoNavigate = true // Trigger navigation to the recording screen
+                        }
+                    }
                 }
             }
-            .navigationBarHidden(true)
-            .background(Color("BackgroundColor").ignoresSafeArea())
-            .onAppear {
-                DispatchQueue.main.async {
-                    viewModel.fetchSessions()
-                }
-                FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).forEach({ url in
-                    let relative = url.relativeString
-                    print("relative url: \(relative)")
-                })
-                let delegate = UIApplication.shared.delegate as! AppDelegate
-                delegate.appDaemon?.removeDeletedEntries()
-        }
-        }
-        .background(Color("BackgroundColor").edgesIgnoringSafeArea(.all))
+            .background(Color("BackgroundColor").edgesIgnoringSafeArea(.all))
         }
     }
 }
+
 
 struct SessionList_Previews: PreviewProvider {
     static var previews: some View {
